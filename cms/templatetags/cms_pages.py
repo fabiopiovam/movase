2970f9f43370f5f64789fff025a94901c3f19e05
laborautonomo <fabio@laborautonomo.org>
2016-03-27 06:27:54 -0300

    [chg] CMS: alter paramaters and template of cms_pages templatetags

--- cms/templatetags/cms_pages.py
+++ cms/templatetags/cms_pages.py
@@ -6,18 +6,20 @@ from cms.models import Category, Page
 register = Library()
 
 @register.inclusion_tag('cms/_list_pages.html')
-def list_pages(category='', limit=10):
+def list_pages(category='', limit=10, orderby='id', section='main'):
     
     try:
         if not category:
-            pages = Page.activated.all()[:limit]
+            pages = Page.activated.all().order_by(orderby)[:limit]
         else:
-            pages = Page.activated.filter(category__slug=category)[:limit]
+            pages = Page.activated.filter(category__slug=category).order_by(orderby)[:limit]
         
     except ObjectDoesNotExist:
         pages = None
     
-    return {'pages': pages,}
+    page_section = "cms/_list_pages_%s.html" % section
+    
+    return {'pages': pages, 'page_section': page_section}
 
 @register.assignment_tag
 def get_page_links(category='', limit=20):
