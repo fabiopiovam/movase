ecf4b98e7186accce6e55ec4c61cc48c9a8e4331
laborautonomo <fabio@laborautonomo.org>
2016-04-04 00:38:19 -0300

    [chg] CMS: new param in 'urls' to get_absolute_url

--- cms/urls.py
+++ cms/urls.py
@@ -1,7 +1,7 @@
 from django.conf.urls import patterns, include, url
 
 urlpatterns = patterns('cms.views',
-    url(r'^category/(?P<category_slug>.*?)/$', 'pages'),
+    url(r'^category/(?P<category_slug>.*?)/$', 'pages', name='category'),
     url(r'^$', 'pages', name='pages'),
     url(r'^(?P<slug>.*?)/$', 'page', name='page'),
 )
