0f805c0f5d8a5c0ab153c807410967ce029a8d33
laborautonomo <fabio@laborautonomo.org>
2016-04-04 00:42:56 -0300

    [chg] Tags: functions to show most commons items

--- tags/models.py
+++ tags/models.py
@@ -4,15 +4,27 @@ from django.db import models
 from django.contrib.contenttypes.models import ContentType
 from django.contrib.contenttypes.generic import GenericForeignKey
 from django.core.urlresolvers import reverse
+from django.db.models import Count
 
 class Tag(models.Model):
     name = models.CharField(max_length=100, unique=True)
+    
+    class Meta:
+        ordering = ('name',)
     
     def __unicode__(self):
         return self.name
     
     def get_absolute_url(self):
         return reverse('tag', kwargs={'tag_name': self.name})
+
+
+class TagManager(models.Manager):
+    def top_tags(self, max=15):
+        top = self.model.objects.values('tag__name').annotate(score=Count('tag')).order_by('-score')[:max]
+        
+        return top
+
 
 class TagItem(models.Model):
     class Meta:
@@ -22,3 +34,5 @@ class TagItem(models.Model):
     content_type = models.ForeignKey(ContentType)
     object_id = models.PositiveIntegerField(db_index=True)
     obj = GenericForeignKey('content_type', 'object_id')
+    
+    objects = TagManager()
