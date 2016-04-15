74fe5f5a3c67be3ecdb30935984f1a0f55b623a1
laborautonomo <fabio@laborautonomo.org>
2016-04-14 22:48:16 -0300

    [chg] add app django.contrib.syndication to rss

--- movase/settings.py
+++ movase/settings.py
@@ -141,6 +141,7 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django.contrib.syndication',
     
     'easy_thumbnails',
     'djangosecure',
